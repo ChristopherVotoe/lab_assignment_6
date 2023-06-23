9dc77446b4e1b82da2888a7554ab851d752a3116
ChristopherV <Chris.Votoe.Official@gmail.com>
2023-06-23 16:58:00 -0400

    Implemented Search Function and Edited README file

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int search(int numbers[], int low, int high, int value) 
+int search(int numbers[], int low, int high, int value)
 {
+	int mid = low + (high - low) / 2;
+	if (numbers[mid] > value)
+	{
+		return search(numbers, low, mid, value);
+	}
+	else if (numbers[mid] < value)
+	{
+		return search(numbers, mid, high, value);
+	}
+	else if (numbers[mid] == value)
+	{
+		return mid;
+	}
+
 	return -1;
 }
 
 void printArray(int numbers[], int sz)
 {
 	int i;
-	printf("Number array : ");
-	for (i = 0;i<sz;++i)
+	printf("Number array: ");
+	for (i = 0; i < sz; ++i)
 	{
-		printf("%d ",numbers[i]);
+		printf("%d ", numbers[i]);
 	}
 	printf("\n");
 }
@@ -19,34 +34,54 @@ void printArray(int numbers[], int sz)
 int main(void)
 {
 	int i, numInputs;
-	char* str;
 	float average;
 	int value;
 	int index;
-	int* numArray = NULL;
+	int *numArray = NULL;
 	int countOfNums;
-	FILE* inFile = fopen("input.txt","r");
+	FILE *inFile = fopen("c:/Users/Potat/OneDrive/Desktop/lab_assignment_6/input.txt", "r");
 
-	fscanf(inFile, " %d\n", &numInputs);
-	
+	if (inFile == NULL)
+	{
+		fprintf(stderr, "Failed to open the file.\n");
+		return 1;
+	}
+
+	fscanf(inFile, "%d\n", &numInputs);
+
 	while (numInputs-- > 0)
 	{
-		fscanf(inFile, " %d\n", &countOfNums);
-		numArray = (int *) malloc(countOfNums * sizeof(int));
+		fscanf(inFile, "%d\n", &countOfNums);
+
+		if (countOfNums <= 0)
+		{
+			fprintf(stderr, "Invalid number of inputs.\n");
+			continue; // Skip this iteration and proceed to the next one
+		}
+
+		numArray = (int *)malloc(countOfNums * sizeof(int));
+		if (numArray == NULL)
+		{
+			fprintf(stderr, "Memory allocation failed.\n");
+			return 1; // or handle the error appropriately
+		}
+
 		average = 0;
 		for (i = 0; i < countOfNums; i++)
 		{
-			fscanf(inFile," %d", &value);
+			fscanf(inFile, "%d", &value);
 			numArray[i] = value;
 			average += numArray[i];
 		}
 
 		printArray(numArray, countOfNums);
+
 		value = average / countOfNums;
 		index = search(numArray, 0, countOfNums - 1, value);
+
 		if (index >= 0)
 		{
-			printf("Item %d exists in the number array at index %d!\n",value, index);
+			printf("Item %d exists in the number array at index %d!\n", value, index);
 		}
 		else
 		{
@@ -57,4 +92,6 @@ int main(void)
 	}
 
 	fclose(inFile);
+
+	return 0;
 }
